ee19ae9a35725ed628efc37a11e8eb3f4d9bb657
Sergo <sergo400ggg@gmail.com>
2021-12-12 06:33:57 +0300

    Finish base framework functional

---
+++ src/components/table/table.template.js
@@ -0,0 +1,51 @@
+const CODES = {
+  A: 65,
+  Z: 90
+}
+
+function createCol(col) {
+  return `
+    <div class="column">${col}</div>
+  `
+}
+
+function createCell() {
+  return `
+    <div class="cell" contenteditable></div>
+  `
+}
+
+function createRow(content, number) {
+  return `
+    <div class="row">
+      <div class="row-info">${number ? number : ''}</div>
+      <div class="row-data">${content}</div>
+    </div>
+  `
+}
+
+function toChar(_, index) {
+  return String.fromCharCode(CODES.A + index)
+}
+
+export function createTable(rowsCount = 15) {
+  const colsCount = CODES.Z - CODES.A + 1
+  const rows = []
+  const cols = new Array(colsCount)
+    .fill('')
+    .map(toChar)
+    .map(createCol)
+    .join('')
+  
+  const cells = new Array(colsCount)
+    .fill(createCell())
+    .join('')
+  
+  rows.push(createRow(cols))
+  
+  for (let i = 0; i < rowsCount; i++) {
+    rows.push(createRow(cells, i+1))
+  }
+  
+  return rows.join('')
+}
